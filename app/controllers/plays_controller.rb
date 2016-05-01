1c09bebf3330e0bc9a511f106e2279c831858d46
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-05-01 12:40:40 -0300

    Add Average Review and User Authentication

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,6 +1,7 @@
 class PlaysController < ApplicationController
 
   before_action :find_play, only: [:show,:edit,:update,:destroy]
+  before_action :authenticate_user!, only: [:new,:edit]
 
   def index
     if params[:category].blank?
@@ -17,6 +18,12 @@ class PlaysController < ApplicationController
   end
 
   def show
+    if @play.reviews.blank?
+      @average_review = 0
+    else
+      @average_review = @play.reviews.average(:rating).round(2)
+    end
+
   end
 
   def edit
